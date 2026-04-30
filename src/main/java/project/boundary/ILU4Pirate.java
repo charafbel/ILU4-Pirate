9a43bc24d9d18769c3a1b4f8b041238520b5f8c5
sosa <114723961+charafbel@users.noreply.github.com>
2026-04-30 22:39:07 +0200

    Creation projet netBeans

---
+++ src/main/java/project/boundary/ILU4Pirate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project.boundary;
+
+/**
+ *
+ * @author belgh
+ */
+public class ILU4Pirate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
