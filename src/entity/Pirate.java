b0b5e7361b51d3475139396ee94633db8f701882
artelien <artelien>
2026-04-30 11:47:25 +0200

    Ajout entite pirate et point

---
+++ src/entity/Pirate.java
@@ -0,0 +1,25 @@
+public class Pirate {
+
+    private int pv;
+    private Point position;
+
+    public Pirate() {
+        pv = 5;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+}
