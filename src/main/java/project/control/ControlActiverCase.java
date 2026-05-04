8401b20524349926071d5b11288f5af91e237a4c
IdirYah <mi_yahiaoui@esi.dz>
2026-05-04 10:59:23 +0200

    controlActiverCase added

--- src/main/java/project/control/ControlActiverCase.java
+++ src/main/java/project/control/ControlActiverCase.java
@@ -1,5 +1,20 @@
 package project.control;
+import project.entity.*;
 
 public class ControlActiverCase {
-
+	public void caseEffect(Pirate joueur,Plateau plateau) {
+		int indice = joueur.getPosition();
+		Case cases = plateau.getCase(indice);
+		if(cases.isSpecial()) {
+			if(cases instanceof CaseBonus) {
+				Equipement equipement = ((CaseBonus) cases).getTypeBonus();
+				joueur.addEquipement(equipement);
+			}else {
+				if(!joueur.hasEquipement()) joueur.setPv(joueur.getPv()-1);
+				else {
+					joueur.removeEquipement(Equipement.BOUCLIER_RESSORT);
+				}
+			}
+		}
+	}
 }
