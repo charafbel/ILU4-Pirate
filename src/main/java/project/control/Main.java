5dfe6ba32e0d3a5ab9d80a94c038eb7728016b1d
Artelien <artelien@MacBook-Pro-de-Artelien.local>
2026-05-04 10:43:23 +0200

    fix

--- src/main/java/project/control/Main.java
+++ src/main/java/project/control/Main.java
@@ -1,8 +1,10 @@
 package project.control;
 
 public class Main {
+
     public static void main(String[] args) {
         LancerDeControler de = new LancerDeControler();
         System.out.println(de.lanceDe());
-    }
+
+	}
 }
