7b33112d93ed2ea649dd57817c41c6c061494c3e
Alban <albangilles625@gmail.com>
2026-05-04 11:16:42 +0200

    ajout du controlleur pour la fin de la partie

--- src/main/java/project/control/ControlVerifierFinPartie.java
+++ src/main/java/project/control/ControlVerifierFinPartie.java
@@ -1,17 +1,34 @@
 package project.control;
 
 import project.entity.Jeu;
+import project.entity.Pirate;
 
 public class ControlVerifierFinPartie {
-	Jeu jeu;
+    Jeu jeu;
 
-	public ControlVerifierFinPartie(Jeu jeu) {
-		super();
-		this.jeu = jeu;
-	}
+    public ControlVerifierFinPartie(Jeu jeu) {
+        super();
+        this.jeu = jeu;
+    }
+    
+    public boolean estFinPartie(){
+        Pirate J1 = jeu.getPirate1();
+        Pirate J2 = jeu.getPirate2();
+        
+        return estFinPartie(J1, J2);
+    }
 
-	public boolean estFinPartie() {
-		// TODO
-		return true;
-	}
+    public boolean estFinPartie(Pirate J1, Pirate J2) {
+        if (J1.getPosition() == 30) {
+            return true;
+        } else if (J2.getPosition() == 30) {
+            return true;
+        } else if (J1.getPv() <= 0) {
+            return true;
+        } else if (J2.getPv() <= 0) {
+            return true;
+        }
+ 
+        return false;
+    }
 }
