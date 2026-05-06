9f073aad404d15b767a87087809c1856d5c6a00b
Artelien <artelien@MacBook-Pro-de-Artelien.local>
2026-05-06 12:33:48 +0200

    Ajout fichier d'entity

--- src/main/java/project/entity/Jeu.java
+++ src/main/java/project/entity/Jeu.java
@@ -2,30 +2,36 @@ package project.entity;
 
 public class Jeu {
 
-    private Plateau plateau;
-    private Pirate pirate1;
-    private Pirate pirate2;
-    private int joueurActuel = 1;
+  private Plateau plateau;
+  private Pirate pirate1;
+  private Pirate pirate2;
+  private int joueurActuel = 1;
+  private De de;
 
-    public Jeu(String nom1, String nom2) {
-        plateau = new Plateau();
-        pirate1 = new Pirate(nom1);
-        pirate2 = new Pirate(nom2);
-    }
+  public Jeu(String nom1, String nom2) {
+    plateau = new Plateau();
+    pirate1 = new Pirate(nom1);
+    pirate2 = new Pirate(nom2);
+    de = new De();
+  }
 
-    public Plateau getPlateau() {
-        return plateau;
-    }
+  public Plateau getPlateau() {
+    return plateau;
+  }
 
-    public Pirate getPirate1() {
-        return pirate1;
-    }
+  public Pirate getPirate1() {
+    return pirate1;
+  }
 
-    public Pirate getPirate2() {
-        return pirate2;
-    }
+  public Pirate getPirate2() {
+    return pirate2;
+  }
 
-    public int getJoueurActuel() {
-        return joueurActuel;
-    }
+  public int getJoueurActuel() {
+    return joueurActuel;
+  }
+
+  public De getDe() {
+    return de;
+  }
 }
